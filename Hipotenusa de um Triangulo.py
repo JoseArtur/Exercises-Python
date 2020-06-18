0a28b3218c0d47711257751a80f25b982c3909ef
José Artur <65734671+JoseArtur@users.noreply.github.com>
2020-06-18 18:35:08 -0300

    Add files via upload

---
+++ Hipotenusa de um Triangulo.py
@@ -0,0 +1,4 @@
+import math
+x=float(input('digite o Valor do Primeiro Cateto: '))
+y=float(input('digite o Valor do Segundo Cateto: '))
+print('O VALOR DA HIPOITENUSA É {:.2f}'.format(math.hypot(x,y)))
